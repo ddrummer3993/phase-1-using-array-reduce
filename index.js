9e371c126c3dfea1e0d0813a2b992ad47ea8e33e
David Drummer <david.b.drummer@gmail.com>
2022-02-08 15:22:50 -0500

    commit tree e5f665187d2712c4992b69e734e553fee9fc0a23

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// create totalBatteries
+const totalBatteries = batteryBatches.reduce((accumulator, element) => {return accumulator += element});
+
+console.log(totalBatteries);
+
